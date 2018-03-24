f33fc3a542ecce174470e7be1c2e5b0c5065b005
Victoria <victoriakaramanova@gmail.com>
2018-03-24 16:36:39 +0200

    comments to p1

--- intro-and-basic-syntax/Problem1_Debit_Card_Number.cs
+++ intro-and-basic-syntax/Problem1_Debit_Card_Number.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 
 namespace homework
 {
-    class Program
+    class Problem1_Debit_Card_Number
     {
         static void Main(string[] args)
         {
+            //A list for storage of the user entries is declared
             List<string> input = new List<string>();
             for (int i = 0; i < 4; i++)
             {
+                //The user enters 4 times sequence of digits
                 var hello = Console.ReadLine();
                 input.Add(hello);
             }
@@ -18,11 +20,14 @@ namespace homework
             {
                 while (input[i].Length < 4)
                 {
+                    /*each digit sequence is furnished with
+                    leading zeros*/
                     input[i] = string.Concat("0", input[i]);
                 }
             }
             foreach (var item in input)
             {
+                //The result is printed on the console
                 Console.Write(item + " ");
             }
         }
