754ec970d7e465408cc5a7a6485ff86a11d931ca
kolyo peev <kolyo.peev@gail.com>
2018-03-24 19:10:16 +0200

    adding solution of Problem 5

---
+++ intro-and-basic-syntax/Problem5_Character_Stats.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int count = 0;
+            List<int> imput = new List<int>();
+            while (count < 4)
+            {
+                imput.Add(int.Parse(Console.ReadLine()));
+                count++;
+            }
+            string health = new String('|', imput[0]);
+            string maxHealth = new String('.', imput[1] - imput[0]);
+            string energy = new String('|', imput[2]);
+            string maxEnergy = new String('.', imput[3] - imput[2]);
+
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Health: |{0}{1}|", health, maxHealth);
+            Console.WriteLine("Health: |{0}{1}|", energy, maxEnergy);
+        }
+    }
+}
