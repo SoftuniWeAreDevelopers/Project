e40bc5ce0f38b9cc5d22f5118ed04c0c7f581cc5
kolyo peev <kolyo.peev@gail.com>
2018-03-24 18:10:00 +0200

    The Solution to Problem 3

---
+++ intro-and-basic-syntax/Program2_Miles_to_Kilometers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine("{0:f2}",(decimal.Parse(input) * 1.60934m));
+
+        }
+    }
+}
