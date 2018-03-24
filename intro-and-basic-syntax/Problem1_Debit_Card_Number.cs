48a9cb426ccc61cebc8717dc55b50002f2229163
kolyo peev <kolyo.peev@gail.com>
2018-03-24 15:17:55 +0200

    Solution of Problem 1 by Kolyo

---
+++ intro-and-basic-syntax/Problem1_Debit_Card_Number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = new List<string>();
+            for (int i = 0; i < 4; i++)
+            {
+                var hello = Console.ReadLine();
+                input.Add(hello);
+            }
+            for (int i = 0; i < input.Count; i++)
+            {
+                while (input[i].Length < 4)
+                {
+                    input[i] = string.Concat("0", input[i]);
+                }
+            }
+            foreach (var item in input)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
